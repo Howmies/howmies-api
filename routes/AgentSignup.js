70cc312ecd571f0b6f99765fd59e5900399355a7
Tobi Akanji <tobia807@gmail.com>
2020-01-01 13:07:47 +0100

    Correct merge issue

--- routes/AgentSignup.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const AgentSignup = require('../controllers/AgentSignup');
-const validate = require('../middleware/request_validator/AgentSignup');
-
-const router = express.Router();
-
-router.post('/signup', validate.agentSignupValidator, AgentSignup.signup);
-
-module.exports = router;
