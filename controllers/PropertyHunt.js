8f6a566e1df88e6170a51f2d941819c34589ce1e
Tobi Akanji <tobia807@gmail.com>
2020-01-22 00:49:50 +0100

    bug(property-hunt): fix error handling [Fixes #25]

--- controllers/PropertyHunt.js
+++ controllers/PropertyHunt.js
@@ -1,7 +1,7 @@
 const { validationResult } = require('express-validator');
 const pool = require('../elephantsql');
 
-exports.hunt = (req, response) => {
+exports.hunt = async (req, response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) { return response.status(422).send({ message: errors.array() }); }
 
@@ -13,70 +13,70 @@ exports.hunt = (req, response) => {
 
   if (features && features.length > 0) {
     const results = [];
-    for (let i = 0; i < features.length; i += 1) {
-      if (location && type) {
-        pool.query(
-          'SELECT * from properties AS p, properties_features AS pf WHERE p.property_id=pf.property_id AND (p.state=$1 OR p.lga=$1) AND p.property_type=$2 AND pf.feature=$3 ORDER BY post_date DESC OFFSET $4 LIMIT 10',
-          [location, type, features[i], pagination],
-          (err, result) => {
-            if (err) {
-              return response.status(500).send({
-                status: err.name,
-                message: err.message,
-              });
-            }
-            if (result.rows.length > 0) {
-              return results.push(result.rows.forEach((e) => e));
-            }
-            return response.status(200).send({
-              status: 'Empty',
-              message: 'No property available',
-            });
-          },
-        );
-      } else if (!type && location) {
-        pool.query(
-          'SELECT * from properties AS p, properties_features AS pf WHERE p.property_id=pf.property_id AND (p.state=$1 OR p.lga=$1) AND pf.feature=$2 ORDER BY post_date DESC OFFSET $3 LIMIT 10',
-          [location, features[i], pagination],
-          (err, result) => {
-            if (err) {
-              return response.status(500).send({
-                status: err.name,
-                message: err.message,
-              });
-            }
-            if (result.rows.length > 0) {
-              return results.push(result.rows.forEach((e) => e));
-            }
-            return response.status(200).send({
-              status: 'Empty',
-              message: 'No property available',
-            });
-          },
-        );
-      } else if (!location && type) {
-        pool.query(
-          'SELECT * from properties AS p, properties_features AS pf WHERE p.property_id=pf.property_id AND p.property_type=$1 AND pf.feature=$2 ORDER BY post_date DESC OFFSET $3 LIMIT 10',
-          [type, features[i], pagination],
-          (err, result) => {
-            if (err) {
-              return response.status(500).send({
-                status: err.name,
-                message: err.message,
-              });
-            }
-            if (result.rows.length > 0) {
-              return results.push(result.rows.forEach((e) => e));
-            }
-            return response.status(200).send({
-              status: 'Empty',
-              message: 'No property available',
-            });
-          },
-        );
-      }
+    results.push(
+      features.forEach(async (feature) => {
+        if (location && type) {
+          await pool.query(
+            'SELECT * from properties AS p, properties_features AS pf WHERE p.property_id=pf.property_id AND (p.state=$1 OR p.lga=$1) AND p.property_type=$2 AND pf.feature_name=$3 ORDER BY post_date DESC OFFSET $4 LIMIT 10',
+            [location, type, feature, pagination],
+            (err, result) => {
+              if (err) {
+                console.log({
+                  status: err.name,
+                  message: err.message,
+                });
+                return 'Internal server error';
+              }
+              if (result.rows.length > 0) {
+                return result.rows.forEach((e) => e);
+              }
+            },
+          );
+        } else if (!type && location) {
+          await pool.query(
+            'SELECT * from properties AS p, properties_features AS pf WHERE p.property_id=pf.property_id AND (p.state=$1 OR p.lga=$1) AND pf.feature_name=$2 ORDER BY post_date DESC OFFSET $3 LIMIT 10',
+            [location, feature, pagination],
+            (err, result) => {
+              if (err) {
+                console.log({
+                  status: err.name,
+                  message: err.message,
+                });
+                return 'Internal server error';
+              }
+              if (result.rows.length > 0) {
+                return result.rows.forEach((e) => e);
+              }
+            },
+          );
+        } else if (!location && type) {
+          await pool.query(
+            'SELECT * from properties AS p, properties_features AS pf WHERE p.property_id=pf.property_id AND p.property_type=$1 AND pf.feature_name=$2 ORDER BY post_date DESC OFFSET $3 LIMIT 10',
+            [type, feature, pagination],
+            (err, result) => {
+              if (err) {
+                console.log({
+                  status: err.name,
+                  message: err.message,
+                });
+                return 'Internal server error';
+              }
+              if (result.rows.length > 0) {
+                return result.rows.forEach((e) => e);
+              }
+            },
+          );
+        }
+      }),
+    );
+
+    if (results.includes('Internal server error')) {
+      return response.status(500).send({
+        status: 'Error',
+        message: 'Internal server error',
+      });
     }
-    if (results.length > 0) {
+    if (!!(results.find((e) => e === null)) && results.length > 0) {
       return response.status(200).send({
         status: 'Success',
         message: 'Property is available',
@@ -85,15 +85,25 @@ exports.hunt = (req, response) => {
         },
       });
     }
-  } else if (location && type) {
+    return response.status(200).send({
+      status: 'Empty',
+      message: 'No property available',
+    });
+  }
+
+  if (location && type) {
     pool.query(
       'SELECT * from properties WHERE (state=$1 OR lga=$1) AND property_type=$2 ORDER BY post_date DESC OFFSET $3 LIMIT 10',
       [location, type, pagination],
       (err, result) => {
         if (err) {
-          return response.status(500).send({
+          console.log({
             status: err.name,
             message: err.message,
+          });
+          return response.status(500).send({
+            status: 'Error',
+            message: 'Internal server error',
           });
         }
         if (result.rows.length > 0) {
@@ -117,9 +127,13 @@ exports.hunt = (req, response) => {
       [location, pagination],
       (err, result) => {
         if (err) {
-          return response.status(500).send({
+          console.log({
             status: err.name,
             message: err.message,
+          });
+          return response.status(500).send({
+            status: 'Error',
+            message: 'Internal server error',
           });
         }
         if (result.rows.length > 0) {
@@ -143,9 +157,13 @@ exports.hunt = (req, response) => {
       [type, pagination],
       (err, result) => {
         if (err) {
-          return response.status(500).send({
+          console.log({
             status: err.name,
             message: err.message,
+          });
+          return response.status(500).send({
+            status: 'Error',
+            message: 'Internal server error',
           });
         }
         if (result.rows.length > 0) {
