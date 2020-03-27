efdf3eb6c73d36bb41b226a967b284ef9c145ca4
Tobi Akanji <tobia807@gmail.com>
2020-03-27 11:51:40 +0100

    bg(cors-error): added Vary header [Finished #42]

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
 
 dotenv.config();
 
@@ -14,16 +15,18 @@ const PostImages = require('./routes/PostImages');
 const RefreshToken = require('./routes/RefreshToken');
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.ACCESS_CONTROL_ALLOW_ORIGIN || '*'}`);
+  res.setHeader('Access-Control-Allow-Origin', process.env.ACCESS_CONTROL_ALLOW_ORIGIN);
+  res.setHeader('Vary', 'Origin');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Set-Cookie');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Expose-Headers', 'Authorization, X-Refresh-Token');
+  res.setHeader('Access-Control-Expose-Headers', 'Authorization');
   next();
 });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(passport.initialize());
 
 const checkAPI = (resMessage = 'OK!') => express.Router().get('/', (req, res) => res.send({ message: resMessage }));
 
