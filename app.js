468c6d7b50eda7395ffeb63f513be0a61c44d526
Tobi Akanji <tobia807@gmail.com>
2020-04-17 22:22:14 +0100

    ft(CORS):add and implement CORS [Finished #49]

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const passport = require('passport');
 
 dotenv.config();
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
