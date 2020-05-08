bfb4c804413e347b299b2cfc1547f8c498b2b9fe
Tobi Akanji <tobia807@gmail.com>
2020-05-08 06:26:08 +0100

    ch(user-signup-test):input the correct table fields[Finishes #69]

--- init.sql
+++ init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS users
 (
+    id integer NOT NULL DEFAULT nextval('"Clients_client_id_seq"'::regclass),
     first_name character varying(24) COLLATE pg_catalog."default" NOT NULL,
     email character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    phone character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    phone character varying(16) COLLATE pg_catalog."default",
     register_date character varying(32) COLLATE pg_catalog."default" NOT NULL,
     other_details text COLLATE pg_catalog."default",
-    password character varying(1024) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(1024) COLLATE pg_catalog."default",
     last_name character varying(24) COLLATE pg_catalog."default" NOT NULL,
-    user_id integer NOT NULL DEFAULT nextval('users_user_id_seq'::regclass),
-    CONSTRAINT users_pkey PRIMARY KEY (user_id),
+    CONSTRAINT clients_pkey PRIMARY KEY (id),
     CONSTRAINT clients_client_email_key UNIQUE (email),
     CONSTRAINT clients_client_phone_number_key UNIQUE (phone)
 )
